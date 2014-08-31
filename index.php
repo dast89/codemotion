1fdaa40bf2ee53c572e3c075bc94005e58a73da7
Dastiw <dastan12@mail.ru>
2014-09-01 02:09:46 +0600

    2kommit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Dastiw
+ * Date: 01.09.14
+ * Time: 2:02
+ * To change this template use File | Settings | File Templates.
+ */
+
+echo 'hello world';
